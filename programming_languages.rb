673bf170393f5c5e93d5f517304272087dded3a3
John Crespi Oncher <johnoncher@gmail.com>
2019-01-03 19:28:46 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,16 @@
 require 'pry'
 def reformat_languages(languages)
+  array = []
   new_hash = {}
-  languages.map! do |style,language|
+  languages.map do |style,language|
+
     language.map do |language, attributes|
       languages[style][language][:style] = [style]
+      new_hash[language] = attributes
+      if new_hash[:javascript]
+        new_hash[:javascript][:style] = [:oo, :functional]
+      end
     end
   end
-  binding.pry
-  
   return new_hash
 end
